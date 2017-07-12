ef5d86c6111427bab8f95d40486c66f9db202cb8
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-07-11 17:55:45 -0700

    get master ready for sprint

--- src/components/navBar.js
+++
@@ -1,8 +0,0 @@
-angular.module('meme-generator')
-
-.controller('NavBarCtrl', function() {})
-.component('navBar', {
-  bindgins: {},
-  controller: 'NavBarCtrl',
-  templateUrl: 'src/templates/navBar.html',
-});
